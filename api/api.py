8a5dc5dc4a45c17b891fba75b8a08282759ad988
priyankasuresh18 <44126949+priyankasuresh18@users.noreply.github.com>
2020-03-29 22:27:29 -0700

    Update api.py

--- api/api.py
+++ api/api.py
@@ -2,16 +2,18 @@ import json
 import io
 import sys
 import re
+import tables
+import numpy
 from flask import Flask
 from keras.models import load_model
 from collections import defaultdict
 
 model = load_model('model_ge_prediction.h5')
-file_h5 = open_file(sys.argv[1])
+file_h5 = tables.open_file(sys.argv[1])
 
 class convert:
 
-    def __init__(self, input_file):
+    def _init_(self, input_file):
         self.file_name = re.sub(r'\.h5$', '', sys.argv[1])
         self.file = input_file
         self.all_groups = []
@@ -35,22 +37,29 @@ class convert:
                 self.group_contents[name].update(array_info)
 
     def json_output(self):
+        print('group_contents: ', self.group_contents)
         main = self.group_contents
         json_file_name = self.file_name + '.json'
         with io.open(json_file_name, 'w', encoding = 'utf-8') as f:
-            f.write(unicode(json.dumps(main, cls = NumpyAwareJSONEncoder, ensure_ascii = False)))
+            f.write(str(json.dumps(main, cls = json_encoder, ensure_ascii = False)))
         f.close()
         return
+
+class json_encoder(self):
+	def default(self, obj):
+		if isinstance(obj, numpy.ndarray):
+			return obj.tolist()
+		return json_encoder(self, obj)
 
 json_data = convert(file_h5)
 contents = json_data.json_output()
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/predicted_stock_values', methods = ['POST'])
 def main():
     if request.method == 'POST':
         return contents
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run()
